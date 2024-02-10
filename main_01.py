85b7593d47e76a5eebad7bc8b629f3fef7f7266b
Serhii <casiomania@proton.me>
2024-02-10 18:54:27 +0200

    initial commit

---
+++ main_01.py
@@ -0,0 +1,3 @@
+# Lesson 02 / Homework 01
+# The user enters three digits from the keyboard. Depending on the user's choice,
+# the program displays the maximum of three, the minimum of three, or the arithmetic mean of the three numbers.
---
+++ main_02.py
@@ -0,0 +1,3 @@
+# Lesson 02 / Homework 02
+# The user enters the number of meters from the keyboard.
+# Depending on the user's choice, the program converts meters to miles, inches, or yards.
