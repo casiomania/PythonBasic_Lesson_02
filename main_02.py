2f1437c93c120937f7e2b9b4456fefe5f2894df1
Serhii <casiomania@proton.me>
2024-02-10 19:57:08 +0200

    02_add_solution

--- main_02.py
+++ main_02.py
@@ -1,3 +1,27 @@
 # Lesson 02 / Homework 02
 # The user enters the number of meters from the keyboard.
 # Depending on the user's choice, the program converts meters to miles, inches, or yards.
+
+# user enters the number of meters
+meters = float(input("Enter number of meters: "))
+
+# choice operation
+selection = input("Choose which system to convert to (miles, inches or yards): ")
+
+# convert to miles
+miles = meters / 1609.34
+
+# convert to inches
+inches = meters * 39.370
+
+# convert to yards
+yards = meters * 1.0936
+
+if selection == "miles":
+    print("Miles: ", miles)
+elif selection == "inches":
+    print("Inches: ", inches)
+elif selection == "yards":
+    print("Yards: ", yards)
+else:
+    print("Incorrect conversion, please enter correct system, miles, inches or yards.")
